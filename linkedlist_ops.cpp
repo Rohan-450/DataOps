6b3e88e18513fb35b6b92155a0fe8d4263c5c78d
Rohan-450 <zombiecpp106@gmail.com>
2023-10-01 17:59:07 +0530

    linked list

--- linkedlist_ops.cpp
+++ linkedlist_ops.cpp
@@ -27,7 +27,7 @@ class linkedlist{
     void deleteFromBeginning();
     void deleteFromEnd();
     void display();
-    void search(int value);
+    bool search(int value);
     void reverse();
     void deleteItem(int);
     void bubbleSort();
@@ -52,6 +52,62 @@ void linkedlist::insertAtEnd(int value){
     cout<<"Inserted"<<value<<"at the end";
 }
 void linkedlist::deleteFromBeginning(){
-    
+    if(!head){
+        cout<<"List is empty"<<endl;
+        return;
+    }
+    Node *temp=head;
+    head=head->next;
+    delete temp;
+    cout<<"Deleted from the beginning"<<endl;
+}
+void linkedlist::deleteFromEnd(){
+    if(!head){
+        cout<<"list is empty"<<endl;
+        return;
+    }
+    if(head->next==NULL){
+        delete head;
+        head=NULL;
+        cout<<"List deleted from end"<<endl;
+        return;
+    }
+    Node *current=head;
+    while(current->next->next!=NULL)
+        current=current->next;
+    delete current->next;
+    current->next=NULL;
+    cout<<"Deleted from end"<<endl;
+}
+void linkedlist::display(){
+    if(head==NULL){
+        cout<<"Empty list"<<endl;
+        return;
+    }
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+bool linkedlist::search(int value){
+    Node *current=head;
+    while(current!=NULL){
+        if(current->data==value)
+            return true;
+        current=current->next;
+    }
+    return false;
+}
+void linkedlist::reverse(){
+    Node *p=head,*q,*r=NULL;
+    while(p!=NULL){
+        q=p;
+        p=p->next;
+        q->next=r;
+        r=q;
+    }
+    head=q;
 }
 
